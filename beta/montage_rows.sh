0aa711684ce335d33e30763e1ff9e37d7dbb86d2
Randy Heiland <randy.heiland@gmail.com>
2025-10-11 08:51:16 -0400

    update

--- beta/montage_rows.sh
+++ beta/montage_rows.sh
@@ -3,4 +3,4 @@ montage -geometry +0+0 -tile 5x1 run06/keep.png run07/keep.png run08/keep.png ru
 montage -geometry +0+0 -tile 5x1 run11/keep.png run12/keep.png run13/keep.png run14/keep.png run15/keep.png row3.png
 montage -geometry +0+0 -tile 5x1 run16/keep.png run17/keep.png run18/keep.png run19/keep.png run20/keep.png row4.png
 montage -geometry +0+0 -tile 5x1 run21/keep.png run22/keep.png run23/keep.png run24/keep.png run25/keep.png row5.png
-montage -geometry +0+0 -tile 1x5 row5.png row4.png row3.png row2.png row1.png pc_phase_diag_1_5.png
+montage -geometry +0+0 -tile 1x5 row5.png row4.png row3.png row2.png row1.png pc_phase_diag_5x5.png
