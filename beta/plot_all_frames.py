5f235866774e652be08ca235e7f6779b44bd527c
Randy Heiland <randy.heiland@gmail.com>
2025-11-09 11:47:40 -0500

    update

---
+++ beta/plot_all_frames.py
@@ -0,0 +1,29 @@
+# cf. montage_monolayer.py
+
+import os
+#import subprocess
+
+# output_dirs = ['out_b0.5_g0.5', 'out_b0.5_g0.6', 'out_b0.5_g0.7', 'out_b0.5_g0.8', 'out_b0.5_g0.9', 'out_b0.6_g0.5', 'out_b0.6_g0.6', 'out_b0.6_g0.7', 'out_b0.6_g0.8', 'out_b0.6_g0.9', 'out_b0.7_g0.5', 'out_b0.7_g0.6', 'out_b0.7_g0.7', 'out_b0.7_g0.8', 'out_b0.7_g0.9', 'out_b0.8_g0.5', 'out_b0.8_g0.6', 'out_b0.8_g0.7', 'out_b0.8_g0.8', 'out_b0.8_g0.9', 'out_b0.9_g0.5', 'out_b0.9_g0.6', 'out_b0.9_g0.7', 'out_b0.9_g0.8', 'out_b0.9_g0.9']
+# output_dirs = ['out_b0.5_g0.5', 'out_b0.5_g0.6']
+output_dirs = []
+
+montage_cmd = "montage -geometry +0+0 -tile 5x5 "
+
+# Roman had beta in X (columns), gamma in Y (rows)
+gamma_vals = [0.5, 0.6, 0.7, 0.8, 0.9]
+print("type(gamma_vals)=", type(gamma_vals))
+print(gamma_vals)
+gamma_r = list(reversed(gamma_vals))
+print("type(gamma_r)=", type(gamma_r))
+print(gamma_r)
+for gamma in gamma_r:
+    for beta in [0.5, 0.6, 0.7, 0.8, 0.9]:
+        output_dir = "out_b" + str(beta) + "_g" + str(gamma)
+        cmd = "python beta/plot_cell_scalars.py -o " + output_dir + " -s beta_or_gamma -f -1 -a -x0 -600 -x1 600 -y0 -600 -y1 600"
+        os.system(cmd)
+
+        montage_cmd += output_dir + "/keep.png "
+
+# montage -geometry +0+0 -tile 5x5 run01/keep.png run02/keep.png run03/keep.png run04/keep.png run05/keep.png row1.png
+montage_cmd += " montage.png"
+print("montage_cmd= ",montage_cmd)
