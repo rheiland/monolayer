f5c54a20ea376c3692b031e39989c7aae0bd89f6
Randy Heiland <randy.heiland@gmail.com>
2025-11-11 16:06:13 -0500

    init

---
+++ analysis/get_csv_5x5_frames.py
@@ -0,0 +1,30 @@
+
+import os
+#import subprocess
+
+# Roman had beta in X (columns), gamma in Y (rows)
+#gamma_vals = [0.5, 0.6, 0.7, 0.8, 0.9]
+#gamma_vals = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+gamma_vals = [0.3, 0.5, 0.7, 0.8, 0.9]
+# gamma_vals = [0.3]
+print("type(gamma_vals)=", type(gamma_vals))
+print(gamma_vals)
+gamma_r = list(reversed(gamma_vals))
+print("type(gamma_r)=", type(gamma_r))
+print(gamma_r)
+
+os.system("rm results.csv")
+os.system("echo 'N,R,A,C,w,g' > results.csv")
+# python analysis/get_cells_csv.py run01_nbrs
+# analysis/metrics cells.csv >> results.csv
+
+for gamma in gamma_r:
+    # for beta in [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]:
+    for beta in [0.6, 0.7, 0.8, 0.9, 0.95]:
+        output_dir = "out_b" + str(beta) + "_g" + str(gamma)
+        cmd = "python analysis/get_cells_csv.py " + output_dir
+        os.system(cmd)
+        cmd = "analysis/metrics cells.csv >> results.csv"
+        os.system(cmd)
+
+print("\n--> results.csv")
