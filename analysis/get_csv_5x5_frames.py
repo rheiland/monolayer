8ad39efa4f5c647dadb745ed40031c134137c71f
Randy Heiland <randy.heiland@gmail.com>
2025-11-11 18:56:51 -0500

    update

--- analysis/get_csv_5x5_frames.py
+++ analysis/get_csv_5x5_frames.py
@@ -6,6 +6,7 @@ import os
 #gamma_vals = [0.5, 0.6, 0.7, 0.8, 0.9]
 #gamma_vals = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
 gamma_vals = [0.3, 0.5, 0.7, 0.8, 0.9]
+# gamma_vals = [0.3, 0.5]
 # gamma_vals = [0.3]
 print("type(gamma_vals)=", type(gamma_vals))
 print(gamma_vals)
@@ -13,8 +14,9 @@ gamma_r = list(reversed(gamma_vals))
 print("type(gamma_r)=", type(gamma_r))
 print(gamma_r)
 
-os.system("rm results.csv")
+# os.system("rm results.csv")
 os.system("echo 'N,R,A,C,w,g' > results.csv")
+os.system("echo 'N,R,A,C,w,g' > results_beta_gamma_params.csv")
 # python analysis/get_cells_csv.py run01_nbrs
 # analysis/metrics cells.csv >> results.csv
 
@@ -27,4 +29,8 @@ for gamma in gamma_r:
         cmd = "analysis/metrics cells.csv >> results.csv"
         os.system(cmd)
 
+        cmd = f"echo {output_dir[4:]} >> results_beta_gamma_params.csv"
+        os.system(cmd)
+
 print("\n--> results.csv")
+print("--> results_beta_gamma_params.csv")
