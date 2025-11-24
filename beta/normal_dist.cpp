866bfe7ac3619bd6c6145ba209f3040b2ee419dd
Randy Heiland <randy.heiland@gmail.com>
2025-11-24 11:08:58 -0500

    init

--- beta/normal_dist.cpp
+++ beta/normal_dist.cpp
@@ -1,6 +1,15 @@
+//  g++-14 normal_dist.cpp -o normal_dist
 #include <iostream>
 #include <random>
 #include <iomanip> // For std::fixed and std::setprecision
+
+thread_local std::mt19937_64 physicell_PRNG_generator; 
+
+double NormalRandom( double mean, double standard_deviation )
+{
+	std::normal_distribution<double> d(mean,standard_deviation);
+	return d(physicell_PRNG_generator); 
+}
 
 int main() {
     // 1. Initialize a random number engine
@@ -14,12 +23,24 @@ int main() {
     const double stddev = 0.4;
     std::normal_distribution<double> distribution(mean, stddev);
 
+    int max_draws = 2.e7;
     // 3. Generate and print some random numbers
     std::cout << std::fixed << std::setprecision(4);
-    std::cout << "Generating 10 samples from N(" << mean << ", " << stddev << "^2):" << std::endl;
-    for (int i = 0; i < 10; ++i) {
-        double number = distribution(generator);
-        std::cout << "Sample " << i + 1 << ": " << number << std::endl;
+    std::cout << "Generating "<< max_draws <<" draws from N(" << mean << ", " << stddev << "^2):" << std::endl;
+    double draw;
+    std::cout << "------- using distribution";
+    for (int i = 0; i < max_draws; ++i) {
+        draw = distribution(generator);
+        if (draw < 0.0)
+            std::cout << "Sample " << i + 1 << ": " << draw << std::endl;
+    }
+
+    std::cout << "------- using NormalRandom";
+    for (int i = 0; i < max_draws; ++i) {
+        // draw = NormalRandom(2.0, 0.25) * 523.6;
+        draw = NormalRandom(2.0, 0.4);
+        if (draw < 0.0)
+            std::cout << "Sample " << i + 1 << ": " << draw << std::endl;
     }
 
     return 0;
